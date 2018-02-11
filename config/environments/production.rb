5e0f9a243a39b7f278a050e2b13aac17f3a61576
Takeshi Kushima <yukicha_0704@yahoo.co.jp>
2018-02-11 03:10:26 +0000

    init

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -89,7 +89,7 @@ Rails.application.configure do
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
   
-  config.action_mailer.default_url_options = { host: '自分のHerokuアプリのドメイン' }
+  config.action_mailer.default_url_options = { host: 'protected-bayou-29328.git' }
 ActionMailer::Base.delivery_method = :smtp
 ActionMailer::Base.smtp_settings =
 {
