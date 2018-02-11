0a620a0069e17fa3a1f27593713d254a06e04fd7
Takeshi Kushima <yukicha_0704@yahoo.co.jp>
2018-02-11 15:18:03 +0000

    migrationfiledelete

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180210174537) do
+ActiveRecord::Schema.define(version: 20180211174537) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -49,4 +49,5 @@ ActiveRecord::Schema.define(version: 20180210174537) do
     t.index ["blog_id"], name: "index_users_on_blog_id"
   end
 
+  add_foreign_key "users", "blogs"
 end
