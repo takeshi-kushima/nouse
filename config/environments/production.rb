8cc6ca6ab1b6e434ecc7675d424be8e12f80ef3e
Takeshi Kushima <yukicha_0704@yahoo.co.jp>
2018-02-16 04:27:56 +0000

    sedgrid

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -89,7 +89,7 @@ Rails.application.configure do
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
   
-  config.action_mailer.default_url_options = { host: 'protected-bayou-29328.git' }
+  config.action_mailer.default_url_options = { host: 'protected-bayou-29328.herokuapp.com' }
 ActionMailer::Base.delivery_method = :smtp
 ActionMailer::Base.smtp_settings =
 {
